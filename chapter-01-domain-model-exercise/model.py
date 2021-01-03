236f38f09c84f983ce91f401ec2c9a0b47823472
Kuei-Jung Hu <akueisara39@gmail.com>
2021-01-03 16:47:43 +0900

    Add a new method can_allocate to pass tests

--- chapter-01-domain-model-exercise/model.py
+++ chapter-01-domain-model-exercise/model.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional
 
+
 # OrderLine is an immutable dataclass with no behavior.
 @dataclass(frozen=True)
 class OrderLine:
@@ -19,3 +20,6 @@ class Batch:
 
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
